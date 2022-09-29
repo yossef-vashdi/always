d9b459c93d577efdb24aaaaac2c73d35910b24e0
Yossi <yova4work@gmail.com>
2022-09-29 11:05:03 +0300

    fixes #1 add simple navbar + mock routes.
    a bit of code cleaning too.
    (comments deleted and next-template code)
    
    (footer- self use)

---
+++ components/layout.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from 'next/link';
+import style from '../styles/layout.module.css';
+
+function Layout({ children }) {
+  return (
+    <>
+      <nav className={style.main}>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/contact-us">Contact us</Link>
+          </li>
+        </ul>
+      </nav>
+      {children}
+    </>
+  );
+}
+
+export default Layout;

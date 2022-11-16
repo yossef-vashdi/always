5bb71fd222dba9a695855b90d830e6302ea82ca4
Yossi <yova4work@gmail.com>
2022-11-16 18:00:14 +0200

    trying to solve bug "module not found.
    im so sad, im guessing, not sure if it will pinpoint the problem

--- pages/fun/movies.js
+++ pages/fun/movies.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import FormMovie from '../../components/fun/formMovie';
-import MoviesComponent from '../../components/fun/MoviesComponent';
+import MoviesComponen from '../../components/fun/MoviesComponent';
 
 //   {console.log('mode: ' + process.env.NODE_ENV)}
 function Movies() {
   return (
     <>
       <FormMovie />
-      <MoviesComponent />
+      <MoviesComponen />
     </>
   );
 }
